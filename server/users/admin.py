b8b80d79a1989985d8513a905c48dc045bdd5b9e
Abdulmateen Tairu <tay2druh@gmail.com>
2020-12-31 14:06:22 +0100

    edited role list

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,3 +1,14 @@
+"""Admin App Customization is done here"""
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+	"""Custom Admin Manager for Custom USer Model"""
+	fieldsets = UserAdmin.fieldsets + (
+		(None, {'fields': ("role",)}),
+	)
+
+
+admin.site.register(User)
--- server/users/models.py
+++ server/users/models.py
@@ -6,8 +6,11 @@ class User(AbstractUser):
 	"""Custom User Model"""
 	role_choices = (
 		('rider', 'rider'),
-		('seller', 'seller')
+		('seller', 'seller'),
+		('admin', 'admin'),
+		('superuser', 'superuser')
 	)
 
-	role = models.CharField(max_length=16, choices=role_choices)
+	DEFAULT_USER_ROLE = 'seller'
 
+	role = models.CharField(max_length=16, choices=role_choices, default=DEFAULT_USER_ROLE)
