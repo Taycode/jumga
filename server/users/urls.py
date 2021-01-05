fbb28d3c367890d2c3fff8dd8902d3bc52ac7b4a
Abdulmateen Tairu <tay2druh@gmail.com>
2021-01-05 02:08:54 +0100

    global products, seller list of products

--- server/users/urls.py
+++ server/users/urls.py
@@ -3,10 +3,13 @@
 
 from django.urls import path, include
 from .views import CreateSuperUserView, CustomRegisterView, UserDetailsView
+from products.views import SellerListProductsView, GlobalListProductsView
+
 
 urlpatterns = [
 	path('registration/', CustomRegisterView.as_view()),
 	path('admin/register/', CreateSuperUserView.as_view()),
 	path('auth/', include('dj_rest_auth.urls')),
-	path('details/', UserDetailsView.as_view())
+	path('details/', UserDetailsView.as_view()),
+	path('my-products/', SellerListProductsView.as_view()),
 ]
