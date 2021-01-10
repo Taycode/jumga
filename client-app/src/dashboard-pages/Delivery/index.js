7b8671b3a366ba12f50cd0e134ac57bf4e8433e5
gold-olar <sam99kupo@gmail.com>
2021-01-10 22:45:14 +0100

    chore: add verification check to sellers app

---
+++ client-app/src/dashboard-pages/Delivery/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Orders = ({ setShowModal }) => {
+  return (
+    <>
+      <Container className="mb-5">
+        <Row>
+          <Col>
+            <h5 className="dashboard-header mb-5"> Deliveries</h5>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Orders;
