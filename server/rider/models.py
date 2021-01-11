f08000e0bb197cefbb9c3e6bd47147b055e8bc58
Abdulmateen Tairu <tay2druh@gmail.com>
2021-01-11 01:32:00 +0100

    riders Delivery model

--- server/rider/models.py
+++ server/rider/models.py
@@ -1,3 +1,17 @@
+"""Models for rider"""
 from django.db import models
+from django.contrib.auth import get_user_model
+from orders.models import Order
+from .choices import status_choices, DEFAULT_STATUS
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Delivery(models.Model):
+	"""Deliveries assigned to a rider"""
+
+	rider = models.ForeignKey(User, on_delete=models.CASCADE)
+	order = models.ForeignKey(Order, on_delete=models.CASCADE)
+	status = models.CharField(max_length=32, choices=status_choices, default=DEFAULT_STATUS)
+	delivered_time = models.DateTimeField(blank=True)
